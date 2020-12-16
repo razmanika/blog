ede2adebf2e8f4793d112b62a6738bf0aa37f31d
razmanika <nikoloz.razmadze.1@btu.edu.ge>
2020-12-16 18:14:53 +0400

    First Commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function posts()
+    {
+        $posts = new Post();
+        
+        
+        return view('posts')->with('posts', $posts->getPost());
+    }
+}
